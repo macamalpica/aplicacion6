3b6a38234bd2d4c935dc5d37ef1e876250ded406
macamalpica <abril.dsaupoo@gmail.com>
2016-05-02 10:50:53 -0500

    1subida
    aplicacion6

---
+++ js/acciones.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+  $(document).ready(function (e){
+  document.addEventListener ("devicereadry", function(){
+
+  $('#beep').tap(function (){
+  navigator.notification.beep (1);
+  });//Tap beep
+
+  $('#vibrar').tap(function (){
+  navigator.notification.vibrate (1000);
+  });//Tap vibrar
+
+  },false); //deviceready
+  }); //Ready
